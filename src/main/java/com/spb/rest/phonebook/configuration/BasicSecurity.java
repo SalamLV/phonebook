b2a233758a4ecdfdd481ec8cef128755491bc193
Aleksejs Demjaņenko <aleksejs.demjanenko@gmail.com>
2024-02-21 14:13:00 +0200

    Small fix for method reference

--- src/main/java/com/spb/rest/phonebook/configuration/BasicSecurity.java
+++ src/main/java/com/spb/rest/phonebook/configuration/BasicSecurity.java
@@ -3,6 +3,7 @@ package com.spb.rest.phonebook.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,7 +11,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
 
 import static org.springframework.security.config.Customizer.withDefaults;
